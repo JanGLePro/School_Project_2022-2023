ba40fcdb202cb42a6b2711dd80c7f95b6586e48b
user <struckovtema@yandex.ru>
2023-03-02 18:18:29 +0300

    pagoda

--- func.py
+++ func.py
@@ -1,9 +1,43 @@
+import requests
+from webbrowser import open_new_tab
+from pathlib import Path
+
+
+def variables(mas):
+    outp = []
+    for i in mas['alternative']:
+        outp.append(i['transcript'])
+    return outp
+
+
+def get_weather(city_name):
+    outp = []
+    API_key = "d879234d7b125e8f3ed1e2d29adbeef9"
+    base_url = "http://api.openweathermap.org/data/2.5/forecast"
+    weather_data = requests.get(base_url, params={'appid': API_key, 'q': city_name, 'cnt': '4',
+                                                  'units': 'metric'}).json()
+
+    if weather_data['message'] == 0:
+        for i in range(4):
+            w = weather_data['list'][i]
+            time = w['dt_txt'].split()[0].split('-')
+            time = '.'.join(time[::-1]) + ' ' + w['dt_txt'].split()[-1]
+            temp = w['main']['temp']
+            feels_like = w['main']['feels_like']
+            humidity = str(w['main']['humidity']) + '%'
+            weather = w['weather'][0]['main']
+            outp.append({'time': time, 'temp': temp,
+                         'feels_like': feels_like, 'humidity': humidity, 'weather': weather})
+        return outp
+
+    return 'неправильное название города'
+
+
 def web_search(inp):
-    from webbrowser import open_new_tab
     open_new_tab(inp)
 
+
 def search_on_disks(name, disk):
-    from pathlib import Path
     k = 0
     flag = False
     a = None
